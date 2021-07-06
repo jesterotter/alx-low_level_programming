9d14d5ed77514e932693e3f8049eae598d9d9bd9
jesterotter <mumbithe97@gmail.com>
2021-07-06 11:57:12 -0700

    task 8 and 9

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * set_string - sets the value of a pointer to a char.
- * @s: double pointer to be copied
- * @to: where s gets set to
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer
+ *
+ * Return : Void function .
  */
 void set_string(char **s, char *to)
 {
